4ca80b1541d4f4e9ab9117b61503035f77412892
yeti103 <sanskarsharma987@gmail.com>
2024-06-12 02:50:08 +0530

    unclickable button created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { useState } from 'react';
 function App() {
+  const[position,setPosition]=useState({top:'50%',left:'50%'});
+  function handleMouseEnter()
+  {
+    const randomTop=Math.floor(Math.random()*(window.innerHeight-50))+'px';
+    const randomLeft=Math.floor(Math.random()*(window.innerWidth-150))+'px';
+    setPosition({top:randomTop,left:randomLeft})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+      className='moving-button'
+      style={{top:position.top,left:position.left}}
+      onMouseEnter={handleMouseEnter}
+      > NO </button>
     </div>
   );
 }

641cd52b7cfeaa5781aec4196ce18413bcd98a61
yeti103 <sanskarsharma987@gmail.com>
2024-06-12 15:04:49 +0530

    initial design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
+import img from './gaalpookie.gif';
+
 function App() {
   const[position,setPosition]=useState({top:'50%',left:'50%'});
   function handleMouseEnter()
@@ -11,11 +13,20 @@ function App() {
   }
   return (
     <div className="App">
+      <div className='image'>
+        <img src={img} alt='gaal-pookie'/>
+      </div>
+      <div className='text'>
+        You love me naa 🥺
+      </div>
+      <button className='yes-button'>
+        YUP 😁
+      </button>
       <button
-      className='moving-button'
+      className='moving-button '
       style={{top:position.top,left:position.left}}
       onMouseEnter={handleMouseEnter}
-      > NO </button>
+      > NO 😱 </button>
     </div>
   );
 }
